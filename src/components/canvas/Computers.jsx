8f5abc2906b61dbd81efa9e5fa81315f84b42aa7
ahmed4567 <ahmed.ashref.1197@gmail.com>
2023-07-07 08:30:50 +0300

    1.3

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -25,36 +25,49 @@ const Computers = ({isMobile}) => {
     </mesh>  
 )
 }
-const ComputersCanvas =()=>{
-  const [isMobile,setIsMobile] = useState(false);
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
   useEffect(() => {
-    const mediaQuery =window.matchMedia("(max-width: 500px)")
-    setIsMobile(mediaQuery.matches)
-    const handleMediaQueryChange =(event)=>{
-      setIsMobile(event.matches)
-    }
-    mediaQuery.addEventListener("change",handleMediaQueryChange)
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
     return () => {
-      mediaQuery.removeEventListener('change',handleMediaQueryChange)
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
     };
   }, []);
 
   return (
-    <Canvas 
-      frameLoop="demand"
-      shadows 
-      camera={{position:[20,3,5],fov:25}}
-      gl={{preserveDrawingBuffer:true}}
-      >
+    <Canvas
+      frameloop='demand'
+      shadows
+      dpr={[1, 2]}
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls 
-        enableZoom={false}
-        maxPolarAngle={Math.PI /2}
-        minPolarAngle={Math.PI /2}
-         />
-         <Computers isMobile/>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
       </Suspense>
-      <Preload all/>
-</Canvas>
-)}
+
+      <Preload all />
+    </Canvas>
+  );
+};
 export default ComputersCanvas 
